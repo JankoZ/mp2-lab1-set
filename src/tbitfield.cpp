6090625bd515124acf6e411d0707d18dee0ffede
JankoZ <maxsya2012@mail.ru>
2022-10-06 20:10:27 +0300

    Edit #1

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -13,86 +13,154 @@ static TBitField FAKE_BITFIELD(1);
 
 TBitField::TBitField(int len)
 {
+    if (len <= 0) throw 0;
+    BitLen = len;
+    MemLen = (BitLen / (sizeof(TELEM) * 8)) + 1;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++) pMem[i] = 0;
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+    BitLen = bf.BitLen;
+    MemLen = bf.MemLen;
+    pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++) pMem[i] = bf.pMem[i];
 }
 
 TBitField::~TBitField()
 {
+    delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
-    return FAKE_INT;
+    if ((n < 0) || (n >= BitLen)) throw 0;
+
+    return int(n / (sizeof(TELEM) * 8));
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
-    return FAKE_INT;
+    if ((n < 0) || (n >= BitLen)) throw 0;
+
+    return 1 << (n % (8 * sizeof(TELEM) - 1));
 }
 
-// доступ к битам битового поля
-
+#pragma region access to bits of a bitfield
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return FAKE_INT;
+    return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+    if (n < 0 || n >= BitLen) throw 0;
+    pMem[GetMemIndex(n)] = pMem[GetMemIndex(n)] | GetMemMask(n);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
+    if (n < 0 || n >= BitLen) throw 0;
+    pMem[GetMemIndex(n)] = pMem[GetMemIndex(n)] & (~GetMemMask(n));
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return FAKE_INT;
+    return (pMem[GetMemIndex(n)] & GetMemMask(n));;
 }
+#pragma endregion
 
-// битовые операции
-
+#pragma region bit operations
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
-    return FAKE_BITFIELD;
+    if (this != &bf)
+    {
+        delete[]pMem;
+        BitLen = bf.BitLen;
+        MemLen = bf.MemLen;
+        pMem = new TELEM[MemLen];
+        for (int i = 0; i < MemLen; i++) pMem[i] = bf.pMem[i];
+    }
+
+    return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
 {
-  return FAKE_INT;
+    if (BitLen != bf.BitLen) return 0;
+    else for (int i = 0; i < MemLen; i++) if (pMem[i] != bf.pMem[i]) return 0;
+
+    return 1;
 }
 
 int TBitField::operator!=(const TBitField &bf) const // сравнение
 {
-  return FAKE_INT;
+    if (BitLen != bf.BitLen) return 1;
+    else for (int i = 0; i < MemLen; i++) if (pMem[i] != bf.pMem[i]) return 1;
+
+    return 0;
 }
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
-    return FAKE_BITFIELD;
+    int temp;
+    if (BitLen >= bf.BitLen) temp = BitLen;
+    else temp = bf.BitLen;
+
+    TBitField tempField(temp);
+    for (int i = 0; i < tempField.MemLen; i++) tempField.pMem[i] = bf.pMem[i] | pMem[i];
+
+    return tempField;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
-    return FAKE_BITFIELD;
+    int temp;
+    if (BitLen >= bf.BitLen) temp = BitLen;
+    else temp = bf.BitLen;
+
+    TBitField tempField(temp);
+    for (int i = 0; i < tempField.MemLen; i++) tempField.pMem[i] = bf.pMem[i] & pMem[i];
+
+    return tempField;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
-    return FAKE_BITFIELD;
+    int length = BitLen;
+    TBitField tempField(length);
+
+    for (int i = 0; i < BitLen; i++)
+        if (GetBit(i) == 0) tempField.SetBit(i);
+        else tempField.ClrBit(i);
+
+    return tempField;
 }
+#pragma endregion
 
-// ввод/вывод
-
+#pragma region I/O
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
+    int i = 0;
+    char temp;
+
+    while (1) {
+        istr >> temp;
+        if (temp == '0') bf.ClrBit(i++);
+        else if (temp == '1') bf.SetBit(i++);
+        else break;
+    }
+
     return istr;
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+    for (int i = 0; i < bf.BitLen; i++)
+        if (bf.GetBit(i)) ostr << '1';
+        else ostr << '0';
+
     return ostr;
 }
+#pragma endregion
